7c8815343938439f53a20a681b24febba29711e7
Jozsef Sztojka <jsztojka2@gmail.com>
2020-05-28 20:34:45 +0200

    change backend url

--- client/src/APIService.js
+++ client/src/APIService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/api/trackers';
+const url = 'https://timetracker-v2.herokuapp.com:5000/api/trackers';
 
 class APIService {
     static getTrackers() {
